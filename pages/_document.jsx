445bdc6c6b4785ea431723bd66d1c9f558da7d13
CarlosAl21 <calvarado2731@uta.edu.ec>
2025-12-02 19:49:43 -0500

    correccion de error

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,15 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-export default function Document() {
-  return (
-    <Html lang="es">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
-        {/* ...otros meta/link si hace falta... */}
-      </Head>
-      <body><Main /><NextScript /></body>
-    </Html>
-  );
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          {/* Mantener vacío o incluir aquí enlaces globales si los necesitas */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
